82ce12c99090cf5c41e8d2aa097c77ea74560abe
Sindre Sorhus <sindresorhus@gmail.com>
2018-11-21 15:36:59 +0700

    Minor style tweaks to TS definition

--- index.d.ts
+++ index.d.ts
@@ -6,6 +6,7 @@ export interface Options {
 	 * Applies a function to the JSON string before parsing.
 	 */
 	beforeParse?: BeforeParse;
+
 	/**
 	 * Prescribes how the value originally produced by parsing is transformed, before being returned.
 	 * See the [`JSON.parse` docs](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/parse#Using_the_reviver_parameter) for more.
